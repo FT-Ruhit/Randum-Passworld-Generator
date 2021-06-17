887e3be3130808a56e59aa35c5c0e7499e7596a5
FT. Ruhit <78923385+FT-Ruhit@users.noreply.github.com>
2021-06-17 14:10:48 +0600

    Update Randum Passworld Generator.c

--- Randum Passworld Generator.c
+++ Randum Passworld Generator.c
@@ -1,32 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h.>
+#include <string.h>
 
-/*
-**********************************************************
-
-  chenge the value of num to change the character number
-
-**********************************************************
-*/
-int num = 12;
+char randumpassgen()
+{
+    srand(time(0));
+    char numbers[] = "0123456789";
+    char smletters[] = "abcdefghijklmnoqprstuvwyzx";
+    char capletters[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+    char symbols[] = "!@#$%^&*?";
+    // Add more if you wnat more complex password
+    int num, randomizer, items;
+    printf("Enter the How many you wnat your password to be?\n");
+    scanf("%d", &num);
+    char password[num];
+    //change the value of num according to your arrays
+    items = 4;
+    randomizer = rand()%num;
+    // Loop for creating password
+    for (int i = 0; i < num; i++)
+    {
+        switch (randomizer)
+        {
+        case 1:
+            password[i] =  numbers[rand()%strlen(numbers)];
+            randomizer = rand()%items;
+            break;
+        case 2:
+            password[i] = smletters[rand()%strlen(smletters)];
+            randomizer = rand()%items;
+            break;
+        case 3:
+            password[i] = capletters[rand()%strlen(capletters)];
+            randomizer = rand()%items;
+            break;
+        // If you want to add more add loginc here
+        default:
+            password[i] = symbols[rand()%strlen(symbols)];
+            randomizer = rand()%items;
+            break;
+        }
+    }
+    // printing the password
+    puts(password);
+}
 
 int main()
 {
-    srand(time(NULL));
-    for (int i = 0; i < num; i++)
-    {
-        int ranD;
-        ranD = rand() % 176 + 1;
-        if (ranD >= 40)
-        {
-            printf("%c", ranD);
-        }
-        else if (ranD < 40)
-        {
-            i--;
-        }
-    }
-
+    randumpassgen();
     return 0;
 }
